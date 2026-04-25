af7221b42edc92cf1577ff366e9f2a3286ff37a3
Mateusz Baginski <cziken58@gmail.com>
2026-04-25 09:34:31 +0200

    fix: adjust typings

--- npm_package/test-utils/context/wait-for-test-context.ts
+++ npm_package/test-utils/context/wait-for-test-context.ts
@@ -9,5 +9,5 @@ export const DEFAULT_TEST_CONTEXT_ID = 'test-context';
  * Waits for the test context to be registered in the EditorsRegistry.
  */
 export function waitForTestContext(id: string = DEFAULT_TEST_CONTEXT_ID) {
-  return ContextsRegistry.the.waitFor(id, 2_500);
+  return ContextsRegistry.the.waitFor(id);
 }
--- npm_package/test-utils/editor/wait-for-test-editor.ts
+++ npm_package/test-utils/editor/wait-for-test-editor.ts
@@ -13,7 +13,7 @@ export const DEFAULT_TEST_EDITOR_ID: EditorId = 'test-editor';
  * Waits for the test editor to be registered in the EditorsRegistry.
  */
 export function waitForTestEditor<E extends Editor>(id: EditorId = DEFAULT_TEST_EDITOR_ID): Promise<E> {
-  return EditorsRegistry.the.waitFor<E>(id, 3_500);
+  return EditorsRegistry.the.waitFor<E>(id);
 }
 
 /**
